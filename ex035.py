1d5720c0ed862298235509eff1344d6a5e672d6f
Rafael Rodrigues Gebra <rafaelrodriguesgebra@gmail.com>
2022-10-17 11:26:47 -0300

    Apendendo Python

---
+++ ex035.py
@@ -0,0 +1,10 @@
+print('-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=')
+print('-=-=-=-=-=-= Analizando triangulos -=-=-=-=-=-=-')
+print('-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=')
+print()
+l1 = float(input('Digite o primeiro valor: '))
+l2 = float(input('Digite o segundo valor: '))
+l3 = float(input('Digiete o terceiro valor: '))
+if l1  l2 and l2  l3 or l3 > l1
+
+if l2 > l3 and l3 > l1 or

6ae1b399e1d0e8808873c522f194aedfdbc547f5
Rafael Rodrigues Gebra <rafaelrodriguesgebra@gmail.com>
2022-11-25 08:54:04 -0300

    Aprendendo Python

--- ex040.py
+++ ex040.py
@@ -2,6 +2,7 @@ nota1 = float(input('Digite a primeira nota do aluno: '))
 nota2 = float(input('Digite a segunda nota do aluno: '))
 media = (nota1 + nota2)/2
 if 5 >= media:
+    print('A sua primeira nota e {} e a sua segunda nota e {} e a sua media e {} entao voce esta '.format(nota1, nota2, media))
     media = 'Reprovado'
 elif (5 < media) and (6.9 > media):
     media = 'Recuperacao'

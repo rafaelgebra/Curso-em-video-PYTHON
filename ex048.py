3473a14786e53d5cebb0db81e461bdaa1e84d562
Rafael Rodrigues Gebra <rafaelrodriguesgebra@gmail.com>
2022-12-13 20:37:43 -0300

    Estudando Python

--- ex048.py
+++ ex048.py
@@ -2,8 +2,12 @@
 # impares que sao multiplos de tres e que se encontram no #
 # intervalo de 1 ate 500 #
 from time import sleep
-for c in range(1*0, 500, 3):
-    if c > 0:
-        print(c)
-        sleep(0.5)
-print('FIM')
+soma = 0
+cont = 0
+for c in range(1, 501, 2):
+    if c % 3 == 0:
+        cont += 1
+        soma += c
+        print(c, end=' ')
+        sleep(0.01)
+print('Soma de {} numeros que sao divisiveis por 3 e {} '.format(cont, soma,))

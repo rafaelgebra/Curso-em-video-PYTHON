7123041ea06aa36ca504c6de36addb90de9c2762
Rafael Rodrigues Gebra <rafaelrodriguesgebra@gmail.com>
2022-12-13 08:07:40 -0300

    Aprendendo Python

---
+++ ex047.py
@@ -0,0 +1,7 @@
+# Crie um prigrama que mostre noa tela #
+# todos os numeros pares que estao entre 1 e 50 #
+from time import sleep
+for c in range(1+1,50+1, ):
+    print(c)
+    sleep(0.2)
+print('FIM')

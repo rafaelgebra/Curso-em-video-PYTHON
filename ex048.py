6a4da00532f762b0cb3084c6f16eea79c73e0f78
Rafael Rodrigues Gebra <rafaelrodriguesgebra@gmail.com>
2022-12-13 08:08:30 -0300

    Aprendendo Python

---
+++ ex048.py
@@ -0,0 +1,9 @@
+# Faca um programa que colcule a soma entre totos os numeros #
+# impares que sao multiplos de tres e que se encontram no #
+# intervalo de 1 ate 500 #
+from time import sleep
+for c in range(1*0, 500, 3):
+    if c > 0:
+        print(c)
+        sleep(0.5)
+print('FIM')

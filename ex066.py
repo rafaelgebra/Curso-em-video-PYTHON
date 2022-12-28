c3099fbf5dba6b9947c7bfd1a10462c9acab5d9c
Rafael Rodrigues Gebra <rafaelrodriguesgebra@gmail.com>
2022-12-28 14:29:22 -0300

    Aprendendo Python

---
+++ ex066.py
@@ -0,0 +1,8 @@
+num = cont = soma = 0
+while True:
+    num = int(input('Ditgite um numero (999 para parar): '))
+    if num == 999:
+        break
+    cont += 1
+    soma += num
+print(f'Foram digitados {cont} numeros e a soma e de {soma}')

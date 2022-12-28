e4931795d45b642f6e0ecd086fc1eb422a274f27
Rafael Rodrigues Gebra <rafaelrodriguesgebra@gmail.com>
2022-12-28 18:09:56 -0300

    Aprendendo Python

--- ex066.py
+++ ex066.py
@@ -1,5 +1,9 @@
-"""Crie um"""
+"""Crie um programa que leia numeros inteiros pelo teclado.
+O programa so vai parar quando o usuario digitar o valor 999,
+que e o comando de parada. No final, mostra quantos numeros
+foram digitados e qual foi a soma entre elas (desconsidetando o flag)."""
 
+""" Nao precisa por a vareavel 'num' no comeco porque esta com while em loop"""
 
 cont = soma = 0
 while True:

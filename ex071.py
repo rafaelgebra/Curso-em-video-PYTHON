ab3596162c989c8fb7b688076708dd92bfe919b8
Rafael Rodrigues Gebra <rafaelrodriguesgebra@gmail.com>
2023-01-02 17:04:09 -0300

    Aprendento Python

--- ex071.py
+++ ex071.py
@@ -7,7 +7,7 @@ valor = int(input('Qual o valor que você quer sacar? R$ '))
 total = valor
 ced = 50
 totced = 0
-while True: 
+while True:
     if total >= ced:
         total -= ced
         totced += 1

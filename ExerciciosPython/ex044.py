6e268e5322cb1ff2b8ec52d02e5c5ab95db51a17
Rafael Rodrigues Gebra <111754401+rafaelgebra@users.noreply.github.com>
2023-06-27 19:23:38 -0300

    Update_theory.py

--- ExerciciosPython/ex044.py
+++ ExerciciosPython/ex044.py
@@ -50,3 +50,31 @@ elif pag == 4:
         print('Opecao invalida. Tente novamente!')
 else:
     print('Opecao invalida. Tente novamente!')
+
+
+    """
+    
+print('{:=^40}'.format(' Loja Guanabara '))
+preco = float(input('Preço das campas: R$ '))
+print('FORMA DE PAGAMENTO')
+print('[ 1 ] à vista dinheiro/cheque' )
+print('[ 2 ] à vista cartão ')
+print('[ 3 ] 2x no cartão ')
+print('[ 4 ] 3x ou mais no cartão')
+opcao = int(input('Qual é a opção '))
+desconto_Avista = (preco * 10) / 100
+desconto_Cartao = (preco * 5) / 100
+juros = (preco * 20 ) / 100
+if opcao == 1:
+    print('Sua compra de R${:.2f} vai custar R${:.2f} no final com desconto de R${:.2f}' .format(preco, preco - desconto_Avista, desconto_Avista))
+elif opcao == 2:
+    print('Sua compra de R${:.2f} vai custar R${:.2f} no final com desconto de R${:.2f}' .format(preco, desconto_Cartao, preco - desconto_Cartao))
+elif opcao == 3:
+    print('Sua compra de R${:.2f} vai custar R${:.2f} cada parcela, não tem juros' .format(preco, preco/2))
+elif opcao == 4:
+    parcelas = int(input('Quantas parcelas '))
+    print('Sua compra será parcelar em {}x de R${:.2f} COM JUROS.' .format(parcelas, juros))
+    print('Sua compra de R${:.2f} vai custar R${:.2f} no final '.format(preco, preco + juros))
+else:
+    print('Opção invalida')
+    """
--- ExerciciosPython/ex045.py
+++ ExerciciosPython/ex045.py
@@ -2,27 +2,46 @@ from random import randint
 from time import sleep
 itens = ('Pedra', 'Papel', 'Tesoura')
 computador = randint(0,2)
+#print('{}' .format(itens[computador]))
 print('''Suas opcoes:
 [0] PEDRA
 [1] PAPEL
 [2] TESOURA ''')
-jogador = int(input('Qual e a sua jogada '))
+jogador = int(input('Qual e a sua jogada? '))
 print('JO')
-sleep(1)
+sleep(0.5)
 print('KEN')
-sleep(1)
+sleep(0.5)
 print('PO!!!')
-sleep(1)
+sleep(0.5)
 print('-=' * 11)
 print('Computado jogou {}'. format(itens[computador]))
 print('Jogador jogou {}' .format(itens[jogador]))
 print('-=' *20)
-if computador == 0: #PEDRA#
-    if jogador == 0: #PEDRA#
+if computador == 0: #Computador jogou PEDRA
+    if jogador == 0: #PEDRA
         print('EMPATE')
-    elif jogador == 1: #PAPEL#
+    elif jogador == 1: #PAPEL
         print('JOGADOR VENCE')
-    elif jogador == 2: #TESOURA#
+    elif jogador == 2: #TESOURA
         print('COMPUTADOR VENCEU')
+    else:
+        print('JOGADA INVALIDA!!')
+elif computador == 1: #Computador jogou PAPEL
+    if jogador == 1: #PAPEL
+        print('EMPATE')
+    elif jogador == 0:#PEDRA
+        print('COMPUTADOR VENCEU')
+    elif jogador == 2: #TESOURA
+        print('JOGADOR VENDEU')
+    else:
+        print('JOGADA INVALIDA!!')
+elif computador == 2: #Computador jogou TESOURA
+    if jogador == 2: #TESOURA
+        print('EMPATE')
+    elif jogador == 0: #PEDRA
+        print('JOGADOR VENDEU')
+    elif jogador == 1: #PAPEL
+        print('COMPUTADOR VENDEU')
     else:
         print('JOGADA INVALIDA!!')

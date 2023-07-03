2aca04b32eed082a03b957d68b1ba1a7e8e23e88
Rafael Rodrigues Gebra <111754401+rafaelgebra@users.noreply.github.com>
2023-07-03 18:54:09 -0300

    Update_theory.py

--- ExerciciosPython/ex056.py
+++ ExerciciosPython/ex056.py
@@ -23,3 +23,32 @@ mediaidade = somaidade / 4
 print('A media de idade do grupo e de {} anos'.format(mediaidade))
 print('O homem mais velho tem {} anos e se chama {}'.format(maioridadehomem,nomevelho ))
 print('Ao todo sao {} mulheres com menos de 20 anos'.format(totmulher20))
+
+"""
+
+soma_Idade = 0
+media_Idade = 0
+maior_Idade_M = 0
+nome_M_Velho = ''
+total_F_20 = 0
+
+for pessoas in range(1, 5):
+    print('{:-^30} '.format(' {}º PESSOA ') .format(pessoas))
+    nome = str(input('Qual o seu nome? ')).strip()
+    idade = int(input('Qual a sua idade? '))
+    sexo = str(input('Qual o seu sexo? [M/F]: ')).strip()
+    media_Idade += idade
+    if pessoas == 1 and sexo in 'Mm':
+        maior_Idade_M = idade
+        nome_M_Velho = nome
+    if sexo in 'Mm' and idade > maior_Idade_M:
+        maior_Idade_M = idade
+        nome_M_Velho = nome
+    if sexo in 'Ff' and idade < 20:
+        total_F_20 += 1
+
+
+print('A média de idade do grupo é de {} anos'. format(media_Idade / 4))
+print('O homem mais velho tem {} e seu nome é {} ' .format(maior_Idade_M, nome_M_Velho))
+print(' O total de mulheres a baixo de 20 anos é {}' .format(total_F_20))
+"""
--- area_de_teste.py
+++ area_de_teste.py
@@ -1,17 +1,26 @@
-maior = 0
-menor = 0   
-for c in range(1,6):
-    peso = float(input('Peso da {}º pessoa: ' .format(c)))
-    if c == 1:
-        maior = peso
-        menor = peso
-    else:
-        if peso > maior:
-            maior = peso
-        if peso < menor:
-            menor = peso
-print('O maior peso lido foi de {:.2f} Kg' .format(maior))
-print('o menor peso lido foi de {:.2f} Kg' .format(menor))
+
+soma_Idade = 0
+media_Idade = 0
+maior_Idade_M = 0
+nome_M_Velho = ''
+total_F_20 = 0
+
+for pessoas in range(1, 5):
+    print('{:-^30} '.format(' {}º PESSOA ') .format(pessoas))
+    nome = str(input('Qual o seu nome? ')).strip()
+    idade = int(input('Qual a sua idade? '))
+    sexo = str(input('Qual o seu sexo? [M/F]: ')).strip()
+    media_Idade += idade
+    if pessoas == 1 and sexo in 'Mm':
+        maior_Idade_M = idade
+        nome_M_Velho = nome
+    if sexo in 'Mm' and idade > maior_Idade_M:
+        maior_Idade_M = idade
+        nome_M_Velho = nome
+    if sexo in 'Ff' and idade < 20:
+        total_F_20 += 1
 
 
-
+print('A média de idade do grupo é de {} anos'. format(media_Idade / 4))
+print('O homem mais velho tem {} e seu nome é {} ' .format(maior_Idade_M, nome_M_Velho))
+print(' O total de mulheres a baixo de 20 anos é {}' .format(total_F_20))

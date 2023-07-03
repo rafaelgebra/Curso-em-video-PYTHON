68318489f823d6a007d41d06be35999b880475ad
Rafael Rodrigues Gebra <111754401+rafaelgebra@users.noreply.github.com>
2023-07-03 15:43:27 -0300

    Update_theory.py

--- area_de_teste.py
+++ area_de_teste.py
@@ -1,13 +1,17 @@
 maior = 0
 menor = 0   
 for c in range(1,6):
-    peso = int(input('Peso da {}º pessoa: ' .format(c)))
-    if peso > maior:
+    peso = float(input('Peso da {}º pessoa: ' .format(c)))
+    if c == 1:
         maior = peso
-    if peso < menor:
         menor = peso
-print('O maior peso lido foi de {:.2f}' .format(maior))
-print('o menor peso lido foi de {:.2f}' .format(menor))
+    else:
+        if peso > maior:
+            maior = peso
+        if peso < menor:
+            menor = peso
+print('O maior peso lido foi de {:.2f} Kg' .format(maior))
+print('o menor peso lido foi de {:.2f} Kg' .format(menor))
 
 
 

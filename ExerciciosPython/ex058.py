a47f084b1cfadd3afe9333ce59bc2af1edd451b6
Rafael Rodrigues Gebra <111754401+rafaelgebra@users.noreply.github.com>
2023-07-08 17:33:30 -0300

    Update_theory.py

--- ExerciciosPython/ex058.py
+++ ExerciciosPython/ex058.py
@@ -15,3 +15,28 @@ while not acertou:
         elif jogador > computador:
             print('Menos... Tente mais uma vez')
 print('Acertou com {} tentativas. Parabens!'.format(palpite))
+
+"""
+from random import randint
+from time import sleep
+computador = randint(0,10)
+print(computador)
+print('Sou o seu computador...')
+sleep(1)
+print('Acabei de pensar em um número entre 0 e 10.')
+sleep(1)
+print('Será que você consegue adivinhar qual é?')
+sleep(1)
+pessoa = int(input('Qual o seu palpite?. '))
+tentativas = 0
+while computador != pessoa :
+    tentativas += 1
+    if pessoa > computador:
+        print('Menos ... Tente outra vez.')
+        sleep(1)
+    if pessoa < computador:
+        print('Mais... Tente mais uma vez ')
+        sleep(1)     
+    pessoa = int(input('Qual o seu palpite?. '))
+print('Acertou com {} tentativas. Parabéns!! ' .format(tentativas))
+"""
--- area_de_teste.py
+++ area_de_teste.py
@@ -1,23 +1,21 @@
 from random import randint
 from time import sleep
 computador = randint(0,10)
-print(computador)
 print('Sou o seu computador...')
-sleep(0.1)
+sleep(1)
 print('Acabei de pensar em um número entre 0 e 10.')
-sleep(0.2)
+sleep(1)
 print('Será que você consegue adivinhar qual é?')
-sleep(0.2)
-pessoa = int(input('Qual o seu palpite? '))
-c = 0
-
+sleep(1)
+pessoa = int(input('Qual o seu palpite?. '))
+tentativas = 0
 while computador != pessoa :
-
-    for c in range(1, 11):
-        if c < 10:
-            c += 1     
-            pessoa = int(input('ERRO! Essa foi a tentativa de número {} ' .format(c)))
-        else:
-            pessoa = computador
-    print('Acabou as suas chances. Tente mais tarde!!')
-print('Parabéns você acertou!!')
+    tentativas += 1
+    if pessoa > computador:
+        print('Menos ... Tente outra vez.')
+        sleep(1)
+    if pessoa < computador:
+        print('Mais... Tente mais uma vez ')
+        sleep(1)     
+    pessoa = int(input('Qual o seu palpite?. '))
+print('Acertou com {} tentativas. Parabéns!! ' .format(tentativas))

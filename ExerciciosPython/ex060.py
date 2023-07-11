3fc57ce0c70142c0465b236611e327875d40debc
Rafael Rodrigues Gebra <111754401+rafaelgebra@users.noreply.github.com>
2023-07-11 16:40:50 -0300

    Update_theory.py

--- ExerciciosPython/ex060.py
+++ ExerciciosPython/ex060.py
@@ -10,19 +10,25 @@ for c in range(n, 0, -1):
 print('{}'.format(f))
 
 
-'''n = int(input('Digite um numero para calcular o seu fatorial: '))
-c = n
+"""
+num = int(input('Digite um número: '))
+c = num
 f = 1
-print('Calculando {}! = '.format(n), end='')
+print('Calculando {}! = ' .format(num), end=' ')
 while c > 0:
-    print('{}'.format(c), end='')
-    print(' x ' if c > 1 else ' = ', end='')
+    print('{} '.format(c), end='')
+    print(' x ' if c > 1 else '= ', end='')
     f *= c
     c -= 1
-print('{}'.format(f))'''
+print('{}' .format(f))
+    
+    
+"""
 
-'''
+
+"""
 from math import factorial
-n = int(input('Digite um numero para calcular o seu fatorial: '))
-f = factorial(n)
-print('O fatorial de {} e {}'.format(n, f))'''
+num = int(input('Digite um número para calcular seu Fatorial: '))
+f = factorial(num)
+print('O fatorial de {} é {}' .format(num, f))
+"""
--- area_de_teste.py
+++ area_de_teste.py
@@ -1,22 +1,3 @@
 
-num = int(input('Digite um número: '))
-c = num
-f = 1
-while c > 0:
-    print('{} '.format(c), end='')
-    print(' x ' if c > 1 else '= ', end='')
-    f *= c
-    c -= 1
-print('{}' .format(f))
-    
-    
-
-
-
-"""from math import factorial
-num = int(input('Digite um número para calcular seu Fatorial: '))
-f = factorial(num)
-print('O fatorial de {} é {}' .format(num, f))"""
-
 
 

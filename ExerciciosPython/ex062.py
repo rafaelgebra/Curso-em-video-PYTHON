7712e7a66d357d555168e84113929aab5f0b60ed
Rafael Rodrigues Gebra <111754401+rafaelgebra@users.noreply.github.com>
2023-07-11 18:58:34 -0300

    Update_theory.py

--- ExerciciosPython/ex062.py
+++ ExerciciosPython/ex062.py
@@ -17,3 +17,24 @@ while mais != 0:
         cont += 1
     mais = int(input('\nQuantos termos voce quer ver a mais '))
 print('Processo finalizado com sucesso com {} termos mostrados'.format(total))
+
+
+"""
+primeiro = int(input('Primeiro termo: '))
+razao = int(input('Razão da PA: '))
+termo = primeiro
+cont = 1
+mais_Termos = 10
+total = 0
+while mais_Termos != 0:
+    total += mais_Termos
+    while cont <= total:
+        print('{} -> ' .format(termo), end=' ')
+        termo += razao
+        cont += 1
+    print('PAUSA')
+    mais_Termos = int(input('Quantos termos você quer mostrar a mais? '))
+    
+print('A quantidade de termos são {} ' .format(cont))
+
+"""

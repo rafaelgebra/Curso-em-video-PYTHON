8aa160d1a9524c1d9bbb4b4a2fb3874af86221df
Rafael Rodrigues Gebra <111754401+rafaelgebra@users.noreply.github.com>
2023-07-12 13:16:38 -0300

    Update_theory_ex063.py

--- ExerciciosPython/ex063.py
+++ ExerciciosPython/ex063.py
@@ -30,4 +30,26 @@ while cont <= n:
     t1 = t2
     t2 = t3
 print("fim")
+
 '''
+
+"""
+print('-'*30)
+print('{:^30}'.format('SEGUÊNCIA DE FIBONACCI'))
+print('-'*30)
+num = int(input('Quantos termos você quer mostar: '))
+termo1 = 0
+termo2 = 1
+cont = 3
+print('='*30)
+print('{} -> {} -> '.format(termo1,termo2), end=' ')
+while cont <= num:
+    cont += 1
+    termo3 = termo1 + termo2
+    print('{} -> '.format(termo3), end=' ')
+    termo1 = termo2
+    termo2 = termo3
+print('FIM')
+print('='*30)
+
+"""

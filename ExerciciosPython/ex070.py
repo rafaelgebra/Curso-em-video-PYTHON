752f9260af7b7bf6ecd5ee4d0d7b24cfeff4c7e8
Rafael Rodrigues Gebra <111754401+rafaelgebra@users.noreply.github.com>
2023-07-14 18:02:21 -0300

    Update_theory_ex070.py

--- ExerciciosPython/ex070.py
+++ ExerciciosPython/ex070.py
@@ -21,3 +21,35 @@ while True:
 print(f'O valor total da compra foi R${total} ')
 print(f'Temos {contm} produtos custando mais de 1000,00')
 print(f'O produto mais barato foi {barato} que custa R${menorv}')
+
+
+"""
+print('-'*40)
+print(f'{"LOJA SUPER BARATÃO":^40}')
+print('-'*40)
+total = total_Produto_Caro = menor_Valor = cont = 0
+while True:
+    continuar = ' '
+    produto = str(input('Nome do produto: ')).strip()
+    preco = int(input('Preço: R$ '))
+    total += preco
+    cont += 1
+    if cont == 1:
+        menor_Valor = preco
+        produto_Barato = produto
+    else:
+        if preco < menor_Valor:
+            menor_Valor = preco
+            produto_Barato = produto
+    if preco >= 1000:
+        total_Produto_Caro += 1
+    while continuar not in 'SN':
+        continuar = str(input('Quer continuar? [S/N]')).strip().upper()[0]
+    if continuar == 'N':
+        break
+print(f'{"FIM DO PROGRAMA":-^40}')
+print(f'O total de compra foi de R${total:.2f}.')
+print(f'Temos {total_Produto_Caro} produto custando mais de R$1000,00.')
+print(f'O produto mais barato foi {produto_Barato} que custou R${menor_Valor:.2f}.')    
+
+"""

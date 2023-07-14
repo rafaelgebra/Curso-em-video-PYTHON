13a341f7ea394ce6aad592936cf9a081447e6884
Rafael Rodrigues Gebra <111754401+rafaelgebra@users.noreply.github.com>
2023-07-14 18:02:30 -0300

    Update_theory_ex071.py

--- ExerciciosPython/ex071.py
+++ ExerciciosPython/ex071.py
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
-
-v1 = int(input('Primeiro '))
-v2 = int(input('Segundo '))
-print(v1 + v2 )
-=======
 valor = int(input('Qual o valor que você quer sacar? R$ '))
 total = valor
 ced = 50
@@ -25,8 +19,37 @@ while True:
         if total == 0:
             break
 
-=======
-v1 = int(input('Primeiro '))
-v2 = int(input('Segundo '))
-print(v1 + v2 )
->>>>>>> f9169c674ae7976ea77affd838a9f1c29368bbc6
+
+"""
+print('='*40)
+print(f'{" BANCO PYTHON" :^40}')
+print('='*40)
+valor = int(input('Qual o valor você quer sacar? R$'))
+total = valor
+ced = 100
+totalced = 0
+while True:
+    if total >= ced:
+        total -= ced
+        totalced += 1
+    else:
+        if totalced > 0:
+            print(f'Total de {totalced} cedulas de R${ced} ')
+        if ced == 100:
+            ced = 50
+        elif ced == 50:
+            ced = 20
+        elif ced == 20:
+            ced = 10
+        elif ced == 10:
+            ced = 5
+        elif ced == 5:
+            ced = 2
+        elif ced == 2:
+            ced = 1
+        totalced = 0
+        if total == 0:
+            break
+print('Volte sempre ao BANCO PYTHON!! Tenha um bom dia!')
+
+"""

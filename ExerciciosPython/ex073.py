8d1ad0a1e3c145f35ebb915d4fd38b14d82b4751
Rafael Rodrigues Gebra <111754401+rafaelgebra@users.noreply.github.com>
2023-07-18 18:49:23 -0300

    Updade_theory_ex073.py

--- ExerciciosPython/ex073.py
+++ ExerciciosPython/ex073.py
@@ -15,3 +15,23 @@ print('-=' * 20)
 print(f'Time em ordem alfabética: {sorted(listacom)}')
 print('-=' * 20)
 print(f'Qual a posição do São Paulo {[listacom.index("São Paulo")+1]}')
+
+
+"""
+listacom = ('Palmeiras','Internacional','Fluminense',
+            'Corinthians','Flamengo','Athetico Paranaense',
+            'Alhético Mineiro','Fortaleza','São Paulo','América Fc Sat',
+            'Botafogo','Santos','Goiás','Red Bull Bragantino','Coritiba',
+            'Cuiabá Sat','Ceará','Atletico','Avaí','Juventude')
+print('='*40)
+print(f'Lista de times do brasileirão: \n{listacom}')
+print('='*40)
+print(f'Os 5 primeiros são: \n{listacom[:5]}')
+print('='*40)
+print(f'Os 4 últimos são: \n{listacom[-4:]}')
+print('='*40)
+print(f'Times em ordem alfabéticas: \n{sorted(listacom)}')
+print('='*40)
+print(f'O São paulo está na {listacom.index("São Paulo")+1} posição')
+
+"""

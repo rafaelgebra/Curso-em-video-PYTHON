2591d23063b0981b6f968c17d7027c561418f0f5
Rafael Rodrigues Gebra <111754401+rafaelgebra@users.noreply.github.com>
2023-07-19 16:13:39 -0300

    Update_theory_ex077.py

--- ExerciciosPython/ex077.py
+++ ExerciciosPython/ex077.py
@@ -9,3 +9,28 @@ for p in palavras: # quando se usa o for nessa linha esta indicando que é para 
     for letra in p: #com o for aninhado a outro for, pode indicar que a repetição "letra" vai ser feita separadamente/analizada uma palavra de cada vez
         if letra.lower() in'aeiou': #com esse if faz o teste logico com as vogais, e deixa imprimir só as vogais
                 print(letra, end=' ')
+
+"""
+
+lista = ('APRENDER','PROGRAMAR','LINGUAGEM')
+
+Ex:1
+
+for elemento in lista:
+    print(f'\nNa palavra {elemento.upper()} tem', end=' ')
+    for letra in elemento:
+        if letra in 'AEIOU':
+            print(letra.lower(), end=', ')
+print(f'\n{"FIM":-^40}')
+
+
+Ex:2
+for palavra in lista:
+    print(f'\nNa palavra {palavra.upper()} temos', end=' ')
+    for letra in palavra:
+        if letra in 'AEIOU':
+            print(f'{letra.lower()}', end=' ')
+print('\n')
+
+
+"""

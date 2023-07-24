c2c21f67ef815c28ea4b96a033955e90de4717ed
Rafael Rodrigues Gebra <111754401+rafaelgebra@users.noreply.github.com>
2023-07-24 18:24:38 -0300

    Upadate_theory_ex082.py

--- ExerciciosPython/ex082.py
+++ ExerciciosPython/ex082.py
@@ -15,3 +15,27 @@ print('-'*30)
 print(f'A lista completa é {lista_geral}')
 print(f'A lista de pares é {lista_pares}')
 print(f'A lista de impares é {lista_impares}')
+
+
+"""
+total_Lista = []
+par_Lista = []
+impar_Lista = []
+while True:
+    num = int(input('Digite um número: '))
+    total_Lista.append(num)
+    continuar = ' '
+    while continuar not in 'SsNn':
+        continuar = str(input('Quer continuar? [S/N]: ')).strip()
+    if num % 2 == 0:
+        par_Lista.append(num)
+    elif num % 2 == 1:
+        impar_Lista.append(num)
+    if continuar in 'Nn':
+        break
+print('-='*30)
+print(f'A lista completa é {total_Lista}')
+print(f'A lista de pares é {par_Lista}')
+print(f'A lista de impares é {impar_Lista}')
+
+"""

2ada5b100b885037d744974e3ced2e3cf2d428f3
Rafael Rodrigues Gebra <111754401+rafaelgebra@users.noreply.github.com>
2023-07-24 15:15:17 -0300

    Update_theory_ex078.py

--- ExerciciosPython/ex078.py
+++ ExerciciosPython/ex078.py
@@ -22,3 +22,38 @@ for i, v in enumerate(lista):
     if v == menor:
         print(f'{i}... ', end='')
 print()
+
+
+
+"""
+valores = []
+menor = maior = 0
+for c in range(0, 5):
+    valores.append(int(input(f'Digite o valor para a Posição {c}: ')))
+    if c == 0:
+        menor = maior = valores[c]
+        #posicao_Maior = posicao_Menor = v - Para ter a posição do primeiro valor
+    else:
+        if valores[c] < menor:
+            menor = valores[c]
+            #posicao_Menor = v - Para ter a posição do primeiro valor
+        elif valores[c] > maior:
+            maior = valores[c]
+            #posicao_Maior = v - Para ter a posição do primeiro valor
+print('-'*60)
+print(f'Você digitou os valores {valores}')
+print(f'O maior valor digitado foi {maior} na posição ', end='')
+for i, v in enumerate(valores):
+    #print(f'i {i}')
+    #print(f'v {v}')
+    if valores[i] == maior:
+        print(f'{i}...', end='')
+print(f'\nO menor valor digitado foi {menor} na posição ', end='')
+for i, v in enumerate(valores):
+    if v == menor:
+        print(f'{i}...')
+print()
+
+
+
+"""

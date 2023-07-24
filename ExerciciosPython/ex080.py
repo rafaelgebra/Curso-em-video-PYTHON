e4fb4efd60c1ae80b7b50ea83b8776d26fdefa6c
Rafael Rodrigues Gebra <111754401+rafaelgebra@users.noreply.github.com>
2023-07-24 17:10:11 -0300

    Update_theory_ex080.py

--- ExerciciosPython/ex080.py
+++ ExerciciosPython/ex080.py
@@ -13,3 +13,28 @@ for c in range(0, 5):
                 break
             pos +=1
 print(f'Os valores digitados em ondem foram{tabela} ')
+
+
+"""
+lista_Valores = []
+for c in range(0,8):
+    num = (int(input('Digite um Valor: '))) 
+    if num not in lista_Valores: # Verifica se o número já esta na lista -  Caso não esteja continua
+        if c == 0 or num > lista_Valores[-1]: # também pode ser escrito assim # if c == 0 : 
+            lista_Valores.append(num)                                         #     lista_Valores.append(num)
+            print('Valor adicionado ao final da lista...')                            # elif n > lista_Valores[-1] # ou n > lista[len(lista)-1]: 
+                                                                              #     lista_Valores.append(num)
+        else:
+            pos = 0
+            while pos < len(lista_Valores): # Verifica em qual posição vai ser adicionado o valor
+                if num <= lista_Valores[pos]:
+                    lista_Valores.insert(pos, num)
+                    print(f'Adicionado na posição {pos} da lista...')
+                    break
+                pos += 1
+    else: # Verifica se o número já esta na lista - Caso ja tenha o número da um aviso que não vai adicionar
+        print('Valor já adicionado, não vou adicionar novamente!!')
+print('-'*60)
+print(f'Os valores digitados em ordem crescente foram {lista_Valores}')
+
+"""

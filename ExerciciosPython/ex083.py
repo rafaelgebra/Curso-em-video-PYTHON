cd990128d58396b97cd8b8a4dc11cb8015b7ff86
Rafael Rodrigues Gebra <111754401+rafaelgebra@users.noreply.github.com>
2023-07-25 09:41:05 -0300

    Update_theory_ex083.py

--- ExerciciosPython/ex083.py
+++ ExerciciosPython/ex083.py
@@ -13,3 +13,23 @@ if len(pilha) == 0:
     print('Sua expressão esta valida')
 else:
     print('Sua expressao esta errada')
+
+
+"""
+expr = str(input('Digite uma expressão: '))
+pilha = []
+for simb in expr:
+    if simb == '(':
+        pilha.append('(')
+    elif simb == ')':
+        if len(pilha) > 0:
+            pilha.pop()
+        else:
+            pilha.append(')')
+            break
+if len(pilha) == 0:
+    print('Sua expressão está correta! ')
+else:
+    print('Sua expressão está errada! ')
+
+"""

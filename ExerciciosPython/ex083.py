5da74c762b4deca888de6d6aca28a3d3c26337bf
Rafael Rodrigues Gebra <111754401+rafaelgebra@users.noreply.github.com>
2023-08-07 17:11:08 -0300

    Update_ex083.py

--- ExerciciosPython/ex083.py
+++ ExerciciosPython/ex083.py
@@ -51,6 +51,44 @@ if len(pilha) == 0:
 else:
     print('Sua expressao esta errada')
 
+    
+    EX: 2
+
+
+
+    dados = list()
+temp = list()
+peso_Maior = peso_Menor = 0
+while True:
+    temp.append(str(input('Nome: ')))
+    temp.append(float(input('Peso: ')))
+
+    if len(dados) == 0:
+        peso_Maior = peso_Menor = temp[1]
+    else:
+        if temp[1] > peso_Maior:
+            peso_Maior = temp[1]
+        if temp[1] < peso_Menor:
+            peso_Menor = temp[1]
+    dados.append(temp[:])
+    temp.clear()
+    continuar = ' '
+    while continuar not in 'NnSs':
+        continuar = str(input('Quer continuar? '))
+    if continuar in 'Nn':
+        break
+print(dados)
+print(f'Ao todo você cadastrou {len(dados)}')
+print(f'O maior peso foi de {peso_Maior}. Peso de', end=' ')
+for pessoa in dados:
+    if pessoa[1] == peso_Maior:
+        print(f'{pessoa[0]}', end=', ')
+print()
+print(f'O menor peso foi de {peso_Menor}. Peso de', end= ' ')
+for pessoa in dados:
+    if pessoa[1] == peso_Menor:
+        print(f'{pessoa[0]}', end=', ')
+print()
 
 
 """

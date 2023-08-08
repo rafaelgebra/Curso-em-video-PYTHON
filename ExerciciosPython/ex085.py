33cf791d288d6b192de293101502c8ff553bc732
Rafael Rodrigues Gebra <111754401+rafaelgebra@users.noreply.github.com>
2023-08-08 09:30:57 -0300

    Update_ex085.py

--- ExerciciosPython/ex085.py
+++ ExerciciosPython/ex085.py
@@ -33,4 +33,21 @@ impar.sort()
 print(f'Os valores ímpares digitados forom: {impar}')
 
 
+
+Ex: 2
+
+
+
+lista = [[], []]
+for c in range(1, 8):
+    num = int(input(f'Digite o {c}º valore: '))
+    if num % 2 == 0:
+        lista[0].append(num)
+    else:
+        lista[1].append(num)
+lista[0].sort()
+lista[1].sort()
+print(f'Os valores pares digitados foram {lista[0]}')
+print(f'Os valores ímpares digitados foram {lista[1]}')
+
 """

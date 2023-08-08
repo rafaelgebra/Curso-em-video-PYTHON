c53d7efaceb08eaba60de2e67acb9b84b0b5c832
Rafael Rodrigues Gebra <111754401+rafaelgebra@users.noreply.github.com>
2023-08-08 15:13:14 -0300

    Update_ex089.py

--- ExerciciosPython/ex089.py
+++ ExerciciosPython/ex089.py
@@ -26,3 +26,42 @@ while True:
     else:
         print('Opção invalida! Tente outra vez')
 print('FIM')
+
+
+
+"""
+
+from time import sleep
+lista_Alunos = []
+while True:
+    nome = str(input('Nome: ')).strip()
+    nota1 = float(input('Nota 1: '))
+    nota2 = float(input('Nota 2: '))
+    media = (nota1 + nota2) / 2
+    lista_Alunos.append([nome,[nota1,nota2],media])
+    continuar = ' '
+    while continuar not in 'NnSs':
+        continuar = str(input('Quer continuar? [S/N]'))
+    if continuar in 'Nn':
+        break
+print('-'*50)
+print(f'{"No":<5} {"Nome":<10} {"MÉDIA":>5}')
+for pos, lista in enumerate(lista_Alunos):
+    print(f'{pos:<5} {lista[0]:<10} {lista[2]:>8.1f}')
+print('-'*40)
+while True:
+    print('-'*40)
+    num = int(input('Mostrar notas de qual aluno? [999 finaliza o programa] '))  
+    if num == 999:
+        print('Finalizando...')
+        sleep(1)
+        break
+    if num <= len(lista_Alunos) -1:
+        print(f'Notas de {lista_Alunos[num][0]} são {lista_Alunos[num][1]}')
+    else:
+        print('Opção invalida')
+print('FIM')
+
+
+
+"""

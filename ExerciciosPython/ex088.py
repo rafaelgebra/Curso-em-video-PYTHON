8a993505e7e8a65734a5166c2d7b7732bc7abf3d
Rafael Rodrigues Gebra <111754401+rafaelgebra@users.noreply.github.com>
2023-08-08 15:12:47 -0300

    Update_ex088.py

--- ExerciciosPython/ex088.py
+++ ExerciciosPython/ex088.py
@@ -41,5 +41,60 @@ for c in range(1, num+1):
     sleep(1)
 print(f'{"BOA SORTE":-^40}')
 
+from random import randint
+from time import sleep
+lista_Temp = []
+lista_Final = []
+print('-'*50)
+print(f'{"JOGA NA MEGA SENA":^50}')
+print('-'*50)
+quant = int(input('Quando jogos você quer que eu sorteie? '))
+tot = 1
+while tot <= quant:
+    cont = 0
+    while True:
+        num = randint(1, 60)
+        if num not in lista_Temp:
+            lista_Temp.append(num)
+            cont += 1
+        if cont >= 6:
+            break
+    lista_Temp.sort()
+    lista_Final.append(lista_Temp[:])
+    lista_Temp.clear()
+    tot += 1
+print('-='*3, f'SORTEANDO {quant} JOGOS', '-='*3 )
+for i, lista in enumerate(lista_Final):
+    sleep(1)
+    print(f'Jogo {i+1}: {lista}')
+sleep(1)
+
+
+EX: 2
+
+from random import randint
+from time import sleep
+lista_Temp = []
+lista_Final = []
+quant = int(input('Quando jogos você quer eu eu faça? '))
+total = 1
+while total <= quant:
+    cont = 0
+    while True:
+        num = randint(1, 60)
+        if num not in lista_Temp:
+            lista_Temp.append(num)
+            cont += 1    
+        if cont >= 6:
+            break
+    lista_Temp.sort()
+    lista_Final.append(lista_Temp[:])
+    lista_Temp.clear()
+    total += 1
+for pos, lista in enumerate(lista_Final): 
+    sleep(1)    
+    print(f'O jogo {pos+1}º: {lista}')
+sleep(1)
+
 
 """

e643a741f8eb8196f5a8f4d4d11db455442d9da0
Rafael Rodrigues Gebra <111754401+rafaelgebra@users.noreply.github.com>
2023-08-12 19:40:33 -0300

    Update_ex090.py

--- ExerciciosPython/ex090.py
+++ ExerciciosPython/ex090.py
@@ -14,3 +14,22 @@ print('-'*50)
 for keys, values in dados.items():
     print(f'  - {keys} é igual a {values}')
 
+"""
+aluno = {}
+aluno['nome'] = str(input('Qual o nome do aluno? '))
+aluno['media'] = float(input(f'Qual a média de {aluno["nome"]}? '))
+
+if aluno['media'] >= 7:
+    aluno['situação'] = 'Aprovado'
+elif 5 <= aluno['media'] < 7:
+    aluno['situação'] = 'Recuperação'
+elif aluno['media'] < 5:
+    aluno['situação'] = 'Reprovado'
+else:
+    print('ERRO... Tente novamente!!')
+print('-'*50)
+for keys, values in aluno.items():
+    print(f'{keys} é igual a {values}')
+
+
+"""

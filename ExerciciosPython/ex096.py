1dee2d4c82a4569dc75caf0a4f0c120a511a5df4
Rafael Rodrigues Gebra <111754401+rafaelgebra@users.noreply.github.com>
2023-08-29 19:21:11 -0300

    Update_ex096.py

--- ExerciciosPython/ex096.py
+++ ExerciciosPython/ex096.py
@@ -1,6 +1,6 @@
 from time import sleep
 
-def aria():
+def area(larg, comp):
     total = largura * comprimento
     print(f'A área de um terreno de {largura:.1f}x{comprimento:.1f} é {total:.1f}m²')
     sleep(1)
@@ -13,22 +13,16 @@ def iteracao(txt):
     sleep(1)
 
 
-c = 0 
+
 while True:
     p()
     print(f'{"Controle de Terrenos":^40}')
     p()
     largura = float(input('LARGURA (m): '))
     comprimento = float(input('COMPRIMENTO (m): '))
-    c += 1
-    if c == 4:
-        iteracao('FIM')
-        iteracao('O resultado é')
-        aria()
-    else:
-        iteracao('Carregando...')
-        iteracao('O resultado é')
-        aria()
+    iteracao('Carregando...')
+    iteracao('O resultado é')
+    area(largura, comprimento)
     while True:
         continuar = str(input('Quer continuar? [S/N]')).strip()[0]
         if continuar in 'SsNn':

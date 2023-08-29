46f7c00c6544fb2cd8e27b16d6e050bf1d8344b4
Rafael Rodrigues Gebra <111754401+rafaelgebra@users.noreply.github.com>
2023-08-29 17:56:43 -0300

    Update_ex096.py

--- ExerciciosPython/ex096.py
+++ ExerciciosPython/ex096.py
@@ -1,21 +1,26 @@
 from time import sleep
+
 def aria():
     total = largura * comprimento
     print(f'A área de um terreno de {largura:.1f}x{comprimento:.1f} é {total:.1f}m²')
     sleep(1)
+
 def p():
     print('-'*40)
+
 def iteracao(txt):
-    
     print(txt)
     sleep(1)
-for c in range(0, 2):
+
+
+c = 0 
+while True:
     p()
     print(f'{"Controle de Terrenos":^40}')
     p()
-    largura = int(input('LARGURA (m): '))
-    comprimento = int(input('COMPRIMENTO (m): '))
-    
+    largura = float(input('LARGURA (m): '))
+    comprimento = float(input('COMPRIMENTO (m): '))
+    c += 1
     if c == 4:
         iteracao('FIM')
         iteracao('O resultado é')
@@ -24,4 +29,11 @@ for c in range(0, 2):
         iteracao('Carregando...')
         iteracao('O resultado é')
         aria()
+    while True:
+        continuar = str(input('Quer continuar? [S/N]')).strip()[0]
+        if continuar in 'SsNn':
+            break
+        print('Opção inválida. Digite S ou N. ')
+    if continuar in 'Nn':
+        break
 

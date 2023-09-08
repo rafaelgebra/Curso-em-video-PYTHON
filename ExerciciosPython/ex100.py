c6102e9fbf39463ab2eed2c200fcbec8ed51f6d3
Rafael Rodrigues Gebra <111754401+rafaelgebra@users.noreply.github.com>
2023-09-08 18:27:55 -0300

    Update_ex100.py

--- ExerciciosPython/ex100.py
+++ ExerciciosPython/ex100.py
@@ -6,13 +6,43 @@ def sortear(sorteio):
     for valor in sorteio:
         print(f'{valor}', end=' ', flush=True)
         sleep(0.3)
+        numeros.append(valor)
     print('PRONTO!!')
 def soma_Par():
-    print(f'Somando os valores pares de {sortear}')
-
-
-
+    total = 0
+    print(f'Somando os valores pares de {numeros}, temos', end=' ')
+    for valor in numeros:
+        if valor % 2 == 0:
+            total += valor
+    print(f'{total}')
+numeros = []
 
 #programa principal
 sortear([randint(0, 10), randint(0, 10), randint(0, 10), randint(0, 10), randint(1, 10)])
 soma_Par()
+
+"""
+from random import randint
+from time import sleep
+
+def sorteia(lista):
+    print('Sorteando 5 valores da lista', end=' ')
+    for con in range(0, 5):
+        n = randint(1, 10)
+        lista.append(n)
+        print(f'{n}', end=' ', flush=True)
+        sleep(0.4)
+    print('PRONTO!')
+
+def somaPar(lista):
+    soma = 0
+    for valor in lista:
+        if valor % 2 == 0:
+            soma += valor
+    print(f'Somando os valores pares de {lista}, temos {soma}')
+
+numeros = []
+sorteia(numeros)
+somaPar(numeros)
+
+"""

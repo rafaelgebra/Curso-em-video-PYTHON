44641ff086e739fe2961d254dfe550612d1f9340
Rafael Rodrigues Gebra <111754401+rafaelgebra@users.noreply.github.com>
2023-09-12 09:50:34 -0300

    Create_ex101.py

---
+++ ExerciciosPython/ex101.py
@@ -0,0 +1,43 @@
+# Atividade 1 com docstring
+def voto(ano):
+    """
+    atual: é a data do dia
+    idade: idade de uma pessoa
+    if: comparação se é melhor de 16 anos
+    elif: comparação se é voto opcional 16 e 17 anos ou maior que 65 anos de idade
+    elif: comparação se o voto é obrigatorio 
+    """
+    from datetime import date
+    atual = date.today().year
+    idade = atual - nasc
+    if idade < 16:
+        return f'Com {idade} anos: NÃO VOTA.'
+    elif 16 <= idade <= 17 or idade > 65:
+        return f'COM {idade} anos: VOTO OPCIONAL'
+    elif idade <= 64:
+        return f'Com {idade} anos: VOTO OBRIGATORIO.'
+
+
+#Programa Principal
+nasc = int(input('Em que ano você nasceu? '))    
+print(voto(nasc))
+
+
+# Atividade 2 sem docstring
+"""
+def voto(ano):
+    from datetime import date
+    atual = date.today().year
+    idade = atual - ano
+    if idade < 16:
+        return f'Com {idade} anos: NÃO VOTA.'
+    elif 16 <= idade < 18 or idade > 65:
+        return f'Com {idade} anos: VOTO OPCIONAL.'
+    else:
+        return f'Com {idade} anos: VOTO OBRIGATORIO.'
+
+
+#Programa Principal    
+nasc = int(input('Em que ano você nasceu? '))
+print(voto(nasc))
+"""

6a9f23536e364e8bdce6fa8fb9ae3ec729918a2f
Rafael Rodrigues Gebra <111754401+rafaelgebra@users.noreply.github.com>
2023-09-12 18:49:55 -0300

    Update_ex103.py

---
+++ ExerciciosPython/ex103.py
@@ -0,0 +1,17 @@
+#Faça um programa que tenha uma função chamada ficha(), que recebe dois parâmetros opcionais: o nome de um jogador e quantos gols ele marcou. O progama deverá ser capaz de mostrar a ficha do jogador, mesmo que algum dado não tenha sido informado corretamente.
+
+def ficha(jog='<desconhecido>', gol=0):
+    print(f'O jogador {jog} fez {gol} gols no campeonato')
+    
+
+#Programa pricipal
+n = str(input('Nome do Jogador: ')).capitalize()
+g = str(input('número de Gols: '))
+if g.isnumeric():
+    g = int(g)
+else:
+    g = 0
+if n.strip() == '':
+    ficha(gol=g)
+else:
+    ficha(n, g)

483a7a27eee58bc49baf23ad809e911e41ca740b
Rafael Rodrigues Gebra <111754401+rafaelgebra@users.noreply.github.com>
2023-09-14 16:40:46 -0300

    Update_ex106.py

--- ExerciciosPython/ex106.py
+++ ExerciciosPython/ex106.py
@@ -1,32 +1,40 @@
 # Faça um mini-sistema que ultiliza o interactive Help do python. O usuário vai digitar o comando e p manual vai aparecer. Quando o usuário digitar a palavra 'FIM', o programa se encerrará. Importante: Use cores.
-
+from time import sleep
 c = ('\33[m',           # 0 - sem cores
      '\33[0;30;41m',    # 1 - Vermelho
      '\33[0;30;42m',    # 2 - verde
      '\33[0;30;43m',    # 3 - amarelo
      '\33[0;30;44m',    # 4 - azul
      '\33[0;30;45m',    # 5 - roxo
-     '\33[7;30m',       # 6 - branco
+     '\33[7;30m'        # 6 - branco
      )
 
+
 def ajuda(com):
+    titulo(f'Acessando o manual do comando \'{com} \'', 4)
+    print(c[6], end='')
     help(com)
+    print(c[0], end='')
+    sleep(2)
+
 
 def titulo(msg, cor=0):
     tam = len(msg) +4
-    print(c[cor],end='') 
+    print(c[cor], end='')
     print('~' * tam)
     print(f'  {msg}  ')
     print('~' * tam)
-    print(c[0],end='')
+    print(c[0], end='')
+    sleep(1)
+
 
 #Programa Principal
 comando = ''
 while True:
-    titulo('SISTEMA DE AJUDA', 1)
+    titulo('SISTEMA DE AJUDA', 2)
     comando = str(input('Função ou Biblioteca > '))
     if comando.upper() == 'FIM':
         break
     else:
         ajuda(comando)
-titulo('ATÉ LOGO', 2)
+titulo('ATÉ LOGO', 1)

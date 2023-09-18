f2ea4065686749301175ee04096074b2e3ab1ca0
Rafael Rodrigues Gebra <111754401+rafaelgebra@users.noreply.github.com>
2023-09-18 11:23:05 -0300

    Update_ex096.py

--- ExerciciosPython/ex096.py
+++ ExerciciosPython/ex096.py
@@ -31,3 +31,50 @@ while True:
     if continuar in 'Nn':
         break
 
+#Exemplo sw exercio 2
+
+"""
+from time import sleep
+def mensagem(txt):
+    """
+    -> Faz a leitura da mensagem e deixa formatada
+    :param txt: recebe a mensagem e escreve com linha em baixo
+    :return: sem retorno
+    """
+    tam = len(txt) + 4
+    print('-' * tam)
+    print(f'  {txt}  ')
+    print('-'*tam)
+
+
+def area(l, c):
+    """
+    -> Faz a leitura do terreno
+    :param l: parametro de largura
+    :param c: paramentro de comprimento
+    :return: sem retorno
+    """
+    area = l * c
+    print(f'A área de um terreno {l}x{c} é de {area}')
+
+
+#Programa Principal
+mensagem('Controle de Terreno')
+while True:
+    largura = float(input('LARGURA (m): '))
+    sleep(0.4)
+    comprimento = float(input('COMPRIMENTO (m): '))
+    sleep(0.4)
+    area(largura, comprimento)
+    sleep(1)
+    while True:
+        continuar = str(input('Quer continuar? [S/N]: ')).strip().upper()[0]
+        if continuar in 'SN':
+            break
+        print('Opção invalida! Digite S ou N.')
+    if continuar == 'N':
+        break
+sleep(1)
+print('FIM')
+
+"""

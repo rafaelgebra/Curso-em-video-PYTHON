cb460cc830ca9e307b48ee4b3916cfdba338dc3e
Rafael Rodrigues Gebra <111754401+rafaelgebra@users.noreply.github.com>
2023-09-18 12:30:18 -0300

    Update_ex097.py

--- ExerciciosPython/ex097.py
+++ ExerciciosPython/ex097.py
@@ -9,3 +9,29 @@ def escreva(txt):
 escreva('Rafael')
 escreva('Estudando Python')
 escreva('Curso em Vídeo')
+
+
+"""
+def escreva(txt):
+    tam = len(txt)+4
+    print('~'*tam)
+    print(f'  {txt}  ')
+    print('~'*tam)
+
+
+#Programa Principal
+while True:
+    escreva('Gustavo Guanabara')
+    escreva('Curso em Vídeo no YouTube')
+    escreva('CeV')
+    while True:
+        continuar = str(input('Quer continuar? [S/N]: ')).strip().upper()[0]
+        if continuar in 'NS':
+            break
+        print('Opção invalida!! Digite somente S ou N.')
+    if continuar == 'N':
+        break
+escreva('FIM... Obrigado por participar')
+
+
+"""

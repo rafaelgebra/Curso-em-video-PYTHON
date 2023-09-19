753965c9aef259021f696c316ac817162a28cf72
Rafael Rodrigues Gebra <111754401+rafaelgebra@users.noreply.github.com>
2023-09-19 16:38:49 -0300

    Update_ex100.py

--- ExerciciosPython/ex099.py
+++ ExerciciosPython/ex099.py
@@ -1,4 +1,7 @@
+# Faça um programa que tenha uma função chamada maior(), que receba vários parâmetros com valores inteiros. Seu programa tem que analizar todos os valores e dizer qual dele é o maior.
 from time import sleep
+
+
 def maior(* num):
     print('-='*20)
     print('Analisando os valores passados...')
@@ -24,3 +27,75 @@ maior(4, 7, 0)
 maior(1, 2) 
 maior(6)
 maior()
+
+'''
+from time import sleep
+
+
+def maior(*num):
+    """
+    -> Programa para análise se números
+    :param num: é usada a forma de desempacotamento, por isso foi usado o *.
+    :return: Sem return.
+    """
+    sleep(0.2)
+    print('-='*20)
+    sleep(0.2)
+    print('Analisando os valores passados...', flush=True)
+    sleep(0.2)
+    c = maior_Num = 0
+    for valor in num:
+        print(valor, end=' ', flush=True)
+        sleep(0.2)
+        if c == 0:
+            maior_Num = valor
+        else:
+            if valor > maior_Num:
+                maior_Num = valor
+        c += 1
+    sleep(0.2)
+    print(f'Foram informados {len(num)} valores ao todo')
+    sleep(0.2)
+    print(f'O Maior número informado foi {maior_Num}.')
+    sleep(0.2)
+    print()
+
+
+def maior2(lis):    # Caso sejá usado uma lista(), NÃO pode por o * indicando que são múltiplos números
+    """
+    -> Programa para analize de números
+    :param lis: é usado uma lista() como parâmetro, por isso não tem *
+    :return: Sem return.
+    """
+    c = maior_Num = 0
+    for v in numeros:
+        for valor in numeros:
+            print(valor, end=' ', flush=True)
+            sleep(0.2)
+            if c == 0:
+                maior_Num = valor
+            else:
+                if valor > maior_Num:
+                    maior_Num = valor
+            c += 1
+        sleep(0.2)
+        print(f'Foram informados {len(numeros)} valores ao todo')
+        sleep(0.2)
+        print(f'O Maior número informado foi {maior_Num}.')
+        sleep(0.2)
+        print()
+        break
+
+
+#Programa Principal.
+maior(2, 9, 4, 5, 7, 1)
+maior(4, 7, 0)
+maior(1, 2)
+maior(6)
+maior()
+numeros = []
+for c in range(0, 5):
+    numeros.append(int(input(f'Informe o {c+1}º número: ')))
+maior2(numeros)
+
+'''

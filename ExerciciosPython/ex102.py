c801d3d4e236a32f70721d68d30e2f7f35793e65
Rafael Rodrigues Gebra <111754401+rafaelgebra@users.noreply.github.com>
2023-09-25 17:37:59 -0300

    Update_ex102.py

--- ExerciciosPython/ex102.py
+++ ExerciciosPython/ex102.py
@@ -1,3 +1,4 @@
+
 def fatorial(num=0, show=False):
     """
     -> Calcula o Fatorial de um número
@@ -6,7 +7,7 @@ def fatorial(num=0, show=False):
     :return: O valor fatorial de um número n.
     """
     f = 1
-    for c in  range(num, 0, -1):
+    for c in range(num, 0, -1):
         if show:
             print(c, end='')
             if c > 1:
@@ -22,3 +23,34 @@ def fatorial(num=0, show=False):
 n = int(input('Digite um número: '))
 print(f'O fatorial do numero de {n} é...', end=' ')
 fatorial(n, show=True)
+
+
+
+'''
+from time import sleep
+def fatorial(n, show=False):
+    """
+    -> Calcula o fatorial de um número
+    :param n: O número a ser calculado
+    :param show: (opcional) mostra ou não o calcúlo
+    :return: O valor do Fatorial de um número n
+    """
+    f = 1
+    print('-'*40)
+    for c in range(n, 0, -1):
+        if show:
+            print(c, end='', flush=True)
+            sleep(1)
+            if c > 1:
+                print(f' x ', end='', flush=True)
+            else:
+                print(f' = ', end='', flush=True)
+        f *= c
+    return f
+
+
+num = int(input('Digite um número: '))
+print(fatorial(num, show=True))
+help(fatorial)
+
+'''

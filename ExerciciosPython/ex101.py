da10ba997dd2a3289c7ee1f9e1fb6bd7b4e85a4f
Rafael Rodrigues Gebra <111754401+rafaelgebra@users.noreply.github.com>
2023-09-25 13:58:50 -0300

    Update_ex101.py

--- ExerciciosPython/ex101.py
+++ ExerciciosPython/ex101.py
@@ -1,4 +1,5 @@
 # Atividade 1 com docstring
+#Crie um programa que tenha uma função chamada voto() que vai receber como parametro o ano de nascimento de uma pessoa. retornando um valor literal indicando se uma pessoa tem voto, NEGADO, OPCIONAL ou OBRIGATÓRIO nas eleições
 def voto(ano):
     """
     atual: é a data do dia
@@ -41,3 +42,26 @@ def voto(ano):
 nasc = int(input('Em que ano você nasceu? '))
 print(voto(nasc))
 """
+'''
+def voto():
+    """
+    -> Programa para definir se o voto é obrigatório
+    :return: Sem return
+    """
+    from datetime import date
+    atual = date.today().year
+    nasc = int(input('Digite o seu ano de nascimento: '))
+    idade = atual - nasc
+    if idade < 16:
+        return f'Com {idade} anos: NÃO VOTA'
+    elif 16 <= idade < 18 or idade >= 65:
+        return f'Com {idade} anos: VOTO OPCIONAL'
+    elif 18 <= idade < 65:
+        return f'Com {idade} anos: VOTO OBRIGATÓRIO'
+
+
+#Programa Principal
+print(voto())
+
+
+'''

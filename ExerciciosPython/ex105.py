0acad940d0923b5c6fbc26cdeead406d76c7fc8d
Rafael Rodrigues Gebra <111754401+rafaelgebra@users.noreply.github.com>
2023-09-27 10:32:43 -0300

    Update_ex105.py

--- ExerciciosPython/ex105.py
+++ ExerciciosPython/ex105.py
@@ -15,7 +15,6 @@ def notas(*n, sit=False):
     :param sit: valor opcional, indicando se deve ou não adicionar a situação
     :return: dicionário com váriasinformações sobre a situação da torma.
     """
-
     r = {}
     r['total'] = len(n)
     r['maior'] = max(n)
@@ -33,3 +32,62 @@ def notas(*n, sit=False):
 #Programa Principal
 resp = notas(5.5, 2.5, 9, 8.5, sit=True)
 print(resp)
+
+
+'''
+def aluno():
+    """
+    -> Função que adiciona notas ao nome do aluno
+    :return: Sem return
+    """
+    notas = []
+    classe['aluno'] = str(input('Nome do aluno: ')).capitalize()
+    while True:
+        nota = str(input(f'nota do aluno {classe["aluno"]}: [999] interrompe o programa '))
+        if nota.isnumeric():
+            valor = int(nota)
+            if valor == 999:
+                break
+            notas.append(valor)
+        else:
+            print('\033[0;31mErro! Digite um valor válido.\033[m')
+    classe['notas'] = notas[:]
+    notas.clear()
+
+
+def notasA(*num, sit=True):
+    """
+    -> Função de analize das notas do aluno
+    :param num: Parámetro que vem da função classe()
+    :param sit: Mostrar ou não a situação do aluno (opcional)
+    :return: Retorna os dados dentro do dicionaria aluno{}
+    """
+    total = 0
+    aluno = {}
+    aluno['nome'] = classe['aluno']
+    aluno['total'] = len(classe['notas'])
+    aluno['maior'] = max(classe['notas'])
+    aluno['menor'] = min(classe['notas'])
+    for valor in classe['notas']:
+        total += valor
+    aluno['média'] = total / aluno['total']
+    if sit:
+        if aluno['média'] < 5:
+            aluno['situação'] = 'ruim'
+            return aluno
+        elif 5 < aluno['média'] < 7:
+            aluno['situação'] = 'boa'
+            return aluno
+        elif 7 <= aluno['média'] <= 10:
+            aluno['situação'] = 'ótimo'
+            return aluno
+    else:
+        return aluno
+
+
+#Programa Principal
+classe = {}
+res = notasA(aluno(), sit=True)
+print(res)
+
+'''
